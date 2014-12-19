10ab3b7186a30d9327718246c3923677b124a30a
matteomanzo <imatteo.manzo@gmail.com>
2014-12-19 16:30:25 +0000

    first test passed, code needs to be refactored

--- lib/inject.rb
+++ lib/inject.rb
@@ -1,5 +1,16 @@
 class Array
 
+  def matt_inject
+    copy = self.dup
 
+    memo = self[0]
+    copy.shift
+    puts "memo before loop #{memo}"
+    copy.each do |num|
+      puts "num before loop #{num}"
+      memo = yield(memo, num)
+      puts "memo after loop #{memo}"
+    end
+    memo
+  end
 end
-
--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -6,36 +6,12 @@ describe Array do
   
     it 'can add numbers' do
       expect([1, 2, 3, 4].inject{|memo, num| memo + num}).to eq 10
-      # expect([1, 2, 3, 4].matt_inject{|memo, num| memo + num}).to eq 10
+      expect([1, 2, 3, 4].matt_inject{|memo, num| memo + num}).to eq 10
     end
 
     it 'can minus numbers'do
 
     end
 
-    it 'can multiply numbers' do
-
-    end
-  end
-
-  context 'with a symbol' do
-
-    it 'can add numbers together' do
-      expect([1, 2, 3, 4].inject(:+)).to eq 10
-    end
-  end
-
-  context 'with an argument an block' do
-
-    it 'can add numbers with a starting point' do
-      expect([1, 2, 3, 4].inject(10){|memo, num| memo + num}).to eq 20
-    end
-  end
-
-  context 'with a symbol argument' do
-
-    it 'can add with a starting point and a symbol' do
-      expect([1, 2, 3, 4].inject(10, :+)).to eq 20
-    end
   end
 end

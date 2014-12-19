c6f076d523596c7d42c5e796348682920f4f8ef9
matteomanzo <imatteo.manzo@gmail.com>
2014-12-19 16:36:12 +0000

    can multiply numbers, test passed

--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -13,5 +13,11 @@ describe Array do
       expect([1, 2, 3, 4].inject{|memo, num| memo - num}).to eq -8
       expect([1, 2, 3, 4].matt_inject{|memo, num| memo - num}).to eq -8
     end
+
+    it 'can multiply numbers' do
+      expect([1, 2, 3, 4].inject{|memo, num| memo * num}).to eq 24
+      expect([1, 2, 3, 4].matt_inject{|memo, num| memo * num}).to eq 24
+    end
+
   end
 end

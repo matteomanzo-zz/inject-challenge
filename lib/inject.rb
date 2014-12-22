54d648188a6a2d9328dbdc74405e413fb27cc7b2
matteomanzo <imatteo.manzo@gmail.com>
2014-12-22 19:57:03 +0000

    can add with a starting point and a symbol, test passed

--- lib/inject.rb
+++ lib/inject.rb
@@ -1,16 +1,23 @@
 class Array
 
-  def matt_inject(memo = self[0], &block)
-    block = memo.to_proc if !block || memo.is_a?(Symbol)
+  def matt_inject(memo = self[0], var = nil, &block)
+    
     copy = self.dup
+    
+    block = var.to_proc if var.is_a?(Symbol)
+    
+    block = memo.to_proc if memo.is_a?(Symbol)
     memo = self[0] if memo.is_a?(Symbol)
+
     copy.shift if memo == self[0]
+    
     puts "memo before loop #{memo}"
     copy.each do |num|
       puts "num before loop #{num}"
       memo = block.call(memo, num)
       puts "memo after loop #{memo}"
     end
+    
     memo
   end
 end
